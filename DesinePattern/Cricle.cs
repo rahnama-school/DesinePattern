c92a5825832264fae3e5b73ce9f45c4384200912
Masiha Ahmadi <esafb52@gmail.com>
2017-06-30 19:50:33 -0700

    Add project files.

---
+++ DesinePattern/Cricle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesinePattern
+{
+    public class Cricle : IShape
+    {
+        public void draw()
+        {
+            Console.WriteLine("draw by cricle");
+        }
+    }
+}
